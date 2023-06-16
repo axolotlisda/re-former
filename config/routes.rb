8e3ad023f0a294fa32cf92a295132b0d5e07e0e9
axolotlfish <canlascyrilchristian@gmail.com>
2023-06-16 13:33:43 +0800

    addes edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
    Rails.application.routes.draw do
      get 'users/new'
      get 'users/create'
-      resources :users, only: [:new, :create]
+     get 'users/edit'
+      resources :users, only: [:new, :create, :edit, :update]
    end
